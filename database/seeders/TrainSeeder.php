e3b67a6786f1c5c3030f88d3ea793b98c40dabca
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-05-27 16:40:57 +0200

    ADD: seeder save method

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -17,15 +17,16 @@ class TrainSeeder extends Seeder
 
         for($i = 0; $i < 50; $i++){
             $new_train = new Train();
-            $new_train->company = $faker->company;
-            $new_train->station_of_departure = $faker->city;
-            $new_train->station_of_arrival = $faker->city;
+            $new_train->company = $faker->company();
+            $new_train->station_of_departure = $faker->city();
+            $new_train->station_of_arrival = $faker->city();
             $new_train->time_of_arrival= $faker->dateTimeThisMonth();
             $new_train->time_of_departure = $faker->dateTimeThisMonth();
             $new_train->train_code = $faker->bothify('??-######');
             $new_train->number_of_coaches = $faker->randomDigitNot(0,);
             $new_train->on_schedule = $faker->boolean();
             $new_train->cancelled = $faker->boolean();
+            $new_train->save();
 
             
             
