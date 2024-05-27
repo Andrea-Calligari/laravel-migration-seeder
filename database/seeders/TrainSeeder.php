5dd93926f8860a9d889cbb4644969981dfd2022b
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-05-27 19:25:40 +0200

    ADD: fixed the trainseeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -13,23 +13,20 @@ class TrainSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        // $date = $faker->dateTimeBetween('this year');
+       
 
-        for($i = 0; $i < 50; $i++){
+        for($i = 0; $i < 5; $i++){
             $new_train = new Train();
             $new_train->company = $faker->company();
             $new_train->station_of_departure = $faker->city();
             $new_train->station_of_arrival = $faker->city();
-            $new_train->time_of_arrival= $faker->dateTimeThisMonth();
-            $new_train->time_of_departure = $faker->dateTimeThisMonth();
+            $new_train->time_of_departure = $faker->dateTimeBetween('-1 week', '-1 week')->format('Y-m-d H:i:s');
+            $new_train->time_of_arrival = $faker->dateTimeInInterval( $new_train->time_of_departure, '+2 days')->format('Y-m-d H:i:s');
             $new_train->train_code = $faker->bothify('??-######');
-            $new_train->number_of_coaches = $faker->randomDigitNot(0,);
+            $new_train->number_of_coaches = $faker->numberBetween(5,10);
             $new_train->on_schedule = $faker->boolean();
-            $new_train->cancelled = $faker->boolean();
+            $new_train->cancelled = $new_train->on_schedule ? 0 : $faker->boolean();
             $new_train->save();
-
-            
-            
         }
     }
 }
